925f2af005815459e0b3019d3527926e5a676fb3
Akhil Guntur <55261146+algolg@users.noreply.github.com>
2022-04-06 15:33:26 -0700

    highlighter

--- highlighter.js
+++ highlighter.js
@@ -1,16 +1,30 @@
 const codes = document.getElementsByTagName("code");
 
 function encase(args, color) {
-    const ele = document.createElement.innerHTML =   `<color class=${color}>${arg}</color>`
-    for (let code of codes) {
-        for (let arg of args) {
-            code.innerHTML = code.innerHTML.replace(arg, ele);
+    for (let arg of args) {
+        const ele = document.createElement.innerHTML = `<color class=${color}>${arg}</color>`
+        for (let code of codes) {
+                code.innerHTML = code.innerHTML.replace(arg, ele);
         }
     }
 }
 
-function highligher() {
-    encase(["true"], "blue");
+function regexToArray(args) {
+    array = [];
+    for (let arg of args) {
+        for (let code of codes) {
+            temp = code.innerHTML.replace(arg, (arg) => {
+                array.push(arg);
+            })
+        }
+    }
+    return (new Set(array));
+}
+
+function highlighter() {
+    encase(["true", "false"], "blue");
     encase(["if", "else", "for", "while"], "red");
     encase(["new"], "red");
+    encase(regexToArray([/(?<=\.)(.*?)(?=\()/]), "purple");
+    encase(regexToArray([/[0-9]/]), "blue");
 }
