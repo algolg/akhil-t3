71378c509d5a292afa9d5ad6c6d198c736ea3099
Akhil Guntur <aguntur12@gmail.com>
2022-04-28 01:19:29 -0700

    fix navbar on small screens

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,5 @@
-bar = document.getElementsByClassName("bar");
-barTitle = document.getElementsByClassName("bar-title");
 mainLink = document.getElementsByClassName("main-link");
 function toggleResponsive() {
-    barTitle[0].classList.toggle("bar-title-display");
-    bar[0].classList.toggle("top-responsive");
     for (i=0; i<mainLink.length; i++) {
         mainLink[i].classList.toggle("main-link-responsive");
     }
